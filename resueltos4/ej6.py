4aff1ecf089aef9a4ecd192cb8591ed110829181
Paradox021 <espanacarmelo@gmail.com>
2022-11-09 13:16:59 +0100

    resueltos4

---
+++ resueltos4/ej6.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# crea una función llamada getKeys(data) que reciba un diccionario y devuelva una lista con las claves del diccionario
+# crea otra función llamada getValues(data) que reciba un diccionario y devuelva una lista con los valores del diccionario
+
+def get_keys(data):
+    lista = list(dict.keys(data))
+    return lista
+
+def get_values(data):
+    lista = list(dict.values(data))
+    return lista
+
+
+def test():
+    assert type(get_keys({'rojo':'bermello', 'amarillo':'amarelo', 'verde':'verde'})) is list, "getKeys Should be a list"
+    assert type(get_values({'rojo':'bermello', 'amarillo':'amarelo', 'verde':'verde'})) is list, "getValues Should be a list"
+    assert get_keys({'rojo':'bermello', 'amarillo':'amarelo', 'verde':'verde'}) == ['rojo', 'amarillo', 'verde'], "Should be ['rojo', 'amarillo', 'verde']"
+    assert get_values({'rojo':'bermello', 'amarillo':'amarelo', 'verde':'verde'}) == ['bermello', 'amarelo', 'verde'], "Should be ['bermello', 'amarelo', 'verde']"
+
+
+if __name__ == "__main__":
+    test()
+    print("Everything passed")
